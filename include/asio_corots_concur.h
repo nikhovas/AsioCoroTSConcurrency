ab4e40d901fa57a2d48af5bca2b3591c3cf2c41c
nikhovas <nikhovas@ya.ru>
2020-06-20 23:38:29 +0500

    fix include error

--- include/asio_corots_concur.h
+++ include/asio_corots_concur.h
@@ -6,12 +6,12 @@
 #define COROUTINESTSADDONS_CORO_TS_ASIO_SYNC_H
 
 
-#include "coro_ts_asio_sync/condition_variable.h"
-#include "coro_ts_asio_sync/semaphore.h"
-#include "coro_ts_asio_sync/latch.h"
-#include "coro_ts_asio_sync/barrier.h"
-#include "coro_ts_asio_sync/channel.h"
-#include "coro_ts_asio_sync/sync_group.h"
+#include "asio_corots_concur/condition_variable.h"
+#include "asio_corots_concur/semaphore.h"
+#include "asio_corots_concur/latch.h"
+#include "asio_corots_concur/barrier.h"
+#include "asio_corots_concur/channel.h"
+#include "asio_corots_concur/sync_group.h"
 
 
 #endif //COROUTINESTSADDONS_CORO_TS_ASIO_SYNC_H
